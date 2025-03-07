1e10e74088ccefb290e2c58908b67640eef1c753
hthnhpzy <beanhateyou@gmail.com>
2025-03-08 00:36:56 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function login()
+{
+  let u = username.value
+  let p = password.value
+  if (u=="vy" && p=="123")
+  {
+    window.open("index.html")
+    document.getElementById(compiler).style.display="block"
+  }
+}
